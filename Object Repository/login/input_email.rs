4eb15c1788014100fdba4977eb7e9c1b27c3cb05
unknown <yudananjayaii@gmail.com>
2024-06-22 16:52:59 +0700

    set folder structure and login module

---
+++ Object Repository/login/input_email.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_email</name>
+   <tag></tag>
+   <elementGuidId>8e7f14a5-c52a-4b99-add2-5755952c6f71</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='email']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//input[@name='email']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//input[@name='email']</value>
+      <webElementGuid>f7a72108-5b0a-40d1-8482-4baf9c783137</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
