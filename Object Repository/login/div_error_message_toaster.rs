857d976a862519714268bb56862c1318fce7c24f
unknown <yudananjayaii@gmail.com>
2024-06-22 20:04:09 +0700

    add invalid test case of login journey

---
+++ Object Repository/login/div_error_message_toaster.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_error_message_toaster</name>
+   <tag></tag>
+   <elementGuidId>076e652f-b9a3-4f25-bbc5-0baf96c088eb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='paper-toast error']//div[@class='toast-content']//h3[contains(text(),'${text}')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='paper-toast error']//div[@class='toast-content']//h3[contains(text(),'${text}')]</value>
+      <webElementGuid>7c4f97e0-b0aa-4bb8-aa6f-02ea1afed050</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/login test cases.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5905ff99-26a1-4bed-94dd-8e5ca336af7e</testSuiteGuid>
+   <testCaseLink>
+      <guid>76019786-eb4a-4042-af15-2a668916880a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/team account/login/teid-00001-as a user i can login with valid email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>94a6c281-e17f-41a8-8513-1d5c87ddb43a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/team account/login/teid-00002-as a user i cant login using unregistered credentials</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7d965890-2750-4948-9435-efed9d8ddec1</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
